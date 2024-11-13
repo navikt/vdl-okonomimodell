6b8b5da789939e5029dd53340f875d165cccd359
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2024-11-13 15:57:27 +0100

    en fungerende løsninge - men ikke ferdig

--- infrastructure/souce_table.sql
+++ infrastructure/souce_table.sql
@@ -3,4 +3,4 @@ use role sysadmin;
 create or replace view okonomimodell.oebs.xxrtv_gl_segment_v as
 select * from tlost__oebs_prod.apps.xxrtv_gl_segment_v;
 create or replace view okonomimodell.oebs.xxrtv_gl_hierarki_v as 
-select * from tlost__oebs_prod.apps.xxrtv_gl_hierarki_v;
+select * from regnskap_raw.oebs.xxrtv_gl_hierarki_v__transient;
