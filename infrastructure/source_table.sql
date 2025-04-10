7d90807f8231baf654528a1ec98f40ebefe3fa4f
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2025-04-10 10:11:26 +0200

    flytter til snowflake som kilde

--- infrastructure/source_table.sql
+++ infrastructure/source_table.sql
@@ -5,4 +5,4 @@ select * from tlost__oebs_prod.apps.xxrtv_gl_segment_v;
 create or replace view okonomimodell.oebs.xxrtv_gl_hierarki_v as 
 select * from regnskap_raw.oebs.xxrtv_gl_hierarki_v__transient;
 create or replace view okonomimodell.oebs.xxrtv_gl_hierarki_v__v1 as 
-select * from tlost__oebs_prod.apps.xxrtv_gl_hierarki_v__transient;
+select * from tlost__oebs_prod.apps.xxrtv_gl_hierarki_v;
