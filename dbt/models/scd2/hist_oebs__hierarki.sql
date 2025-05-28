507e924c52d79f4bbecd758cf9aa563f67dd3507
Patrick Pedersen <patrick.pedersen@nav.no>
2025-05-28 14:31:41 +0200

    WIP WIP

--- dbt/models/scd2/hist_oebs__hierarki.sql
+++ dbt/models/scd2/hist_oebs__hierarki.sql
@@ -1,6 +1,6 @@
 {{
     hist(
-        from=source("oebs", "hierarki"),
+        from=ref("tmp_hist_src"),
         entity_key=["hierarchy_code", "flex_value_id"],
         check_cols=[
             "flex_value",
--- dbt/models/scd2/test/test_hist_deletes.sql
+++
@@ -1,7 +0,0 @@
-{{
-    hist_deletes(
-        from=ref("test_scd2_src"),
-        entity_key=["id"],
-        loaded_at="t",
-    )
-}}
